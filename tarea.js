7a28cb45daa98f0f390b8b292fcf2d1b3bc93f25
Mariana Ramírez Acosta <maria.ramirezacosta9@gmail.com>
2022-08-18 17:23:26 -0500

    se avanza tarea de test

--- tarea.js
+++ tarea.js
@@ -1,1 +1,14 @@
 //Hacer 2 ejercicios con su respectivo test.
+
+//Evaluar el área de un triángulo 
+const areaTriangulo = (a,b) => (a*b)/2;
+
+console.log(areaTriangulo(8,4));
+
+//Almacenar funciones
+const area = {
+    areaTriangulo,
+}
+
+//Exportar
+module.exports = area;
---
+++ tarea.test.js
@@ -0,0 +1,5 @@
+const areaTriangulo = require('./tarea');
+
+test ('Obtener Área', () => {
+    expect (calculadora.area(4,2)).toBe(4);
+})
